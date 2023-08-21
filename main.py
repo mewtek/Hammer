2fad9c75c5ccd68fcf8e651f8723dc51035d7708
April Wormwood <mewtek@proton.me>
2023-08-20 17:52:22 -0700

    Add guild to database when joining

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import backend.db
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -16,11 +17,16 @@ intents.guild_messages = True
 
 
 # Bot setup
-client = commands.Bot(command_prefix="h!", intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
 
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user.name}..")
+
+@client.event
+async def on_guild_join(guild: discord.Guild):
+    print(f"Joined new guild {guild.id}.")
+    await backend.db.add_guild_to_database(guild.id, guild.name)
 
 
 @client.command()
