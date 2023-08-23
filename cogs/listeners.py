77c6e44ec262c44f4531f5362486431676287b38
April Wormwood <mewtek@proton.me>
2023-08-23 14:43:39 -0700

    Automatically add users when a message is sent

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -1,14 +1,25 @@
 import backend.db
+import discord
+
 from discord.ext import commands
 
 class Listeners(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         print(f"Joined server {guild.name} (ID {guild.id})")
         await backend.db.add_guild_to_database(guild.id, guild.name)
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        user = message.author
+
+        if user.id == self.bot.user.id:
+            return
+        
+        await backend.db.add_user_to_database(user.id, user.name)
 
 async def setup(bot):
     await bot.add_cog(Listeners(bot))
