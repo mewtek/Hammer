3784a33cea5258bd6b8410a52a10d9b368d17231
April Wormwood <mewtek@proton.me>
2023-08-23 15:03:06 -0700

    cogs.config boilerplate

---
+++ cogs/config.py
@@ -0,0 +1,33 @@
+import discord
+import backend.db
+from discord.ext import commands
+
+
+class Configuration(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.group()
+    async def config(self, ctx: commands.Context):
+        if ctx.invoked_subcommand is None:
+            pass
+
+
+    @config.command()
+    async def enable(self, ctx: commands.Context, setting: str):
+        pass
+
+
+    @config.command()
+    async def disable(self, ctx: commands.Context, setting: str):
+        pass
+
+
+    @config.command()
+    async def set(self, ctx: commands.Context, setting: str):
+        pass
+
+
+
+async def setup(bot):
+    await bot.add_cog(Configuration(bot))
