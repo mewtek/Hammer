7d574c86fe619409990a6dfc627676fcfd60a291
April Wormwood <mewtek@proton.me>
2023-08-22 21:27:44 -0700

    Guild joining listener

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -1,9 +1,14 @@
+import backend.db
 from discord.ext import commands
 
 class Listeners(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        print(f"Joined server {guild.name} (ID {guild.id})")
+        await backend.db.add_guild_to_database(guild.id, guild.name)
 
-def setup(bot):
-    bot.add_cog(Listeners(bot))
+async def setup(bot):
+    await bot.add_cog(Listeners(bot))
