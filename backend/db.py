e16ff4bcc8f8143f736d1bc6e9a647e4cbec2447
April Wormwood <mewtek@proton.me>
2023-08-22 22:25:25 -0700

    Kick & Ban commands

--- backend/db.py
+++ backend/db.py
@@ -117,3 +117,59 @@ async def remove_warning(warning_id: int, guild_id: int) -> bool:
     await db.close()
 
     return True
+
+
+async def add_ban(issued_by: int, issued_to: int, guild_id: int, reason: str = None, expiration: str = None):
+    """
+    Adds a server ban to the database.
+
+    Args:
+        issued_by (int): The ID of the user that issued the ban
+        issued_to (int): The ID of the user the ban was issued to
+        guild_id (int): The ID of the server the ban was issued in
+        reason (str, optional): The reason for the ban. Defaults to None.
+        expiration (str, optional): The amount of the time the ban is valid for. Defaults to None.
+    """
+
+    time_issued = datetime.utcnow()
+    db = await asyncpg.connect(**PSQL_INFO)
+
+    await db.execute('''INSERT INTO ban(issued, issued_by, issued_to, issued_guild) VALUES($1, $2, $3, $4)''',
+                     time_issued, issued_by, issued_to, guild_id)
+    
+    if reason is not None:
+        id = db.fetchval('''SELECT id FROM ban WHERE issued_by = $1 AND issued_to = $2 AND issued_guild = $3''',
+                         issued_by, issued_to, guild_id)    
+        await db.execute('''UPDATE ban SET reason = $1 WHERE id = $2''', reason, id)
+
+    # TODO: Add expiration time
+
+    await db.close()
+    print(f"{issued_to} was banned from {guild_id}")
+
+
+async def remove_ban(user_id: int, guild_id: int) -> bool:
+    """
+    Removes the ban on a user in a specific guild.
+
+    Args:
+        user_id (int): The ID of the user to be unbanned.
+        guild_id (int): The ID of the server that the command was issued in.
+
+    Returns:
+        bool: True or False depending on whether or not the removal was successful.
+    """
+
+    db = await asyncpg.connect(**PSQL_INFO)
+
+    # asyncpg will throw an error if the ban doesn't exist.
+    ban_check = await db.fetchrow("SELECT * FROM ban WHERE issued_to = $1 AND issued_guild = $2", user_id, guild_id)
+    if ban_check is None:
+        return False
+    
+    await db.execute("DELETE FROM ban WHERE issued_to = $1 AND issued_guild = $2", user_id, guild_id)
+    await db.close()
+
+    print(f"{user_id} was unbanned from {guild_id}.")
+
+    return True
--- cogs/admin.py
+++ cogs/admin.py
@@ -30,5 +30,34 @@ class Admin(commands.Cog):
 
         await ctx.reply(f"Successfully removed warning #{warning_id}!")
 
+
+    @commands.command()
+    async def kick(self, ctx: commands.Context, user: discord.Member, reason: str = None):
+        if reason is None:
+            await user.kick(f"KICKED BY {ctx.author.id} -- No reason was provided.")
+        else:
+            await user.kick(f"KICKED BY {ctx.author.id} -- {reason}")
+
+        await ctx.message.add_reaction(u"\u2705")
+    
+    @commands.command()
+    async def ban(self, ctx: commands.Context, user: discord.Member, reason: str = None):
+        issued_by = ctx.message.author
+        issued_to = user.id
+        guild_id = ctx.message.guild.id
+
+        ban = await backend.db.add_ban(issued_by, issued_to, guild_id, reason)
+
+        if ban == False:
+            await ctx.reply(f"Failed to ban {user.name}")
+            return
+        
+        if reason is None:
+            await user.ban(reason = f"Banned by {issued_by} -- No reason was provided.")
+        else:
+            await user.ban(reason = f"Banned by {issued_by} -- {reason}")
+
+        await ctx.message.add_reaction(u"\u2705")
+
 async def setup(bot):
     await bot.add_cog(Admin(bot))
