abf807664481a00003ff4b922f0fda7fbe8b2ec1
mewtek <mewtek@icloud.com>
2023-08-25 02:06:07 -0700

    Function to ban guilds

---
+++ backend/db_admin.py
@@ -0,0 +1,23 @@
+""" Database queries and functions for the owner of the bot """
+import asyncpg
+from backend.db import PSQL_INFO
+
+
+async def add_banned_guild(guild_id: int):
+    """
+    Sets a server's "banned" value in the database to true.
+
+    Args:
+        guild_id (int): The ID of the server that is getting banned
+        purge_guild (bool, optional): Whether or not to purge the server from the database. Defaults to False.
+
+    Returns:
+        bool: Whether or not the ban was successful.
+    """
+
+    db = await asyncpg.connect(**PSQL_INFO)
+
+    await db.execute("UPDATE guild SET banned = true WHERE guild_id = $1", guild_id)
+    await db.close()
+
+    print(f"{guild_id} was banned from using the bot.")
---
+++ cogs/bot_owner.py
@@ -0,0 +1,31 @@
+import discord
+import backend.db_admin
+from discord.ext import commands
+
+
+class BotOwner(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+
+    @commands.command()
+    @commands.is_owner()
+    async def shutdown(self, ctx):
+        print("Shutting down the bot..")
+        await self.bot.close()
+
+
+    @commands.command()
+    @commands.is_owner()
+    async def ban_guild(self, ctx: commands.Context, guild_id: int):
+        guild = await self.bot.fetch_guild(guild_id)
+
+        if guild is None:
+            await ctx.reply("Guild does not exist.")
+            return
+        
+        await backend.db_admin.add_banned_guild(guild_id)
+        await ctx.message.add_reaction(u"\u2705")
+
+async def setup(bot):
+    await bot.add_cog(BotOwner(bot))
