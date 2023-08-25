4099ed5cb87a71a5bd2b34932cd1a54bc0a5151e
mewtek <mewtek@icloud.com>
2023-08-25 01:59:15 -0700

    Function to see if user is banned from a server

--- backend/db.py
+++ backend/db.py
@@ -169,12 +169,11 @@ async def is_user_muted(user_id: int, guild_id: int) -> bool:
     db = await asyncpg.connect(**PSQL_INFO)
     row = await db.fetchrow("SELECT * FROM mute WHERE issued_to = $1 AND issued_guild = $2",
                       user_id, guild_id)
-    
+    await db.close()
+
     if row is None:
-        await db.close()
         return False
     
-    await db.close()
     return True
 
 
@@ -272,6 +271,28 @@ async def get_muted_role_id(guild_id: int) -> int:
     return role_id
 
 
+async def is_user_banned(user_id: int, guild_id: int) -> bool:
+    """
+    Checks whether or not a user is banned in a server.
+
+    Args:
+        user_id (int): The ID of the user in question
+        guild_id (int): The ID of the server they should be banned in
+
+    Returns:
+        bool: Whether or not the user is banned from the server
+    """
+
+    db = await asyncpg.connect(**PSQL_INFO)
+    row = await db.fetchrow("SELECT * FROM ban WHERE issued_to = $1 AND issued_guild = $2",
+                            user_id, guild_id)
+    await db.close()
+
+    if row is None:
+        return False
+    
+    return True
+
 async def add_ban(issued_by: int, issued_to: int, guild_id: int, reason: str = None, expiration: str = None):
     """
     Adds a server ban to the database.
@@ -288,9 +309,8 @@ async def add_ban(issued_by: int, issued_to: int, guild_id: int, reason: str = N
     db = await asyncpg.connect(**PSQL_INFO)
 
     # A user should only be banned once per server
-    ban_check = await db.fetchrow("SELECT * FROM ban WHERE issued_to = $1 AND issued_guild = $2",
-                                  issued_to, guild_id)
-    if ban_check is not None:
+    if (await is_user_banned(issued_to, guild_id)):
+        await db.close()
         return
 
     await db.execute('''INSERT INTO ban(issued, issued_by, issued_to, issued_guild) VALUES($1, $2, $3, $4)''',
@@ -326,8 +346,8 @@ async def remove_ban(user_id: int, guild_id: int) -> bool:
     db = await asyncpg.connect(**PSQL_INFO)
 
     # asyncpg will throw an error if the ban doesn't exist.
-    ban_check = await db.fetchrow("SELECT * FROM ban WHERE issued_to = $1 AND issued_guild = $2", user_id, guild_id)
-    if ban_check is None:
+    if (await is_user_banned(user_id, guild_id) == False):
+        await db.close()
         return False
     
     await db.execute("DELETE FROM ban WHERE issued_to = $1 AND issued_guild = $2", user_id, guild_id)
