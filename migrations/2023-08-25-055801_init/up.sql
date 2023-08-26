200d3d78833ae73a029b785cfcced4b88f0b23d2
mewtek <mewtek@icloud.com>
2023-08-26 01:50:54 -0700

    Refactor guild_id and user_id into id

--- migrations/2023-08-25-055801_init/up.sql
+++ migrations/2023-08-25-055801_init/up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE "user"
 (
-    user_id bigint NOT NULL PRIMARY KEY,
+    id bigint NOT NULL PRIMARY KEY,
     user_name varchar(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE guild
 (
-    guild_id bigint NOT NULL PRIMARY KEY,
+    id bigint NOT NULL PRIMARY KEY,
     guild_name varchar(255) NOT NULL,
     banned boolean NOT NULL DEFAULT false
 );
@@ -22,7 +22,7 @@ CREATE TABLE guild_settings
     dm_user_on_action boolean NOT NULL DEFAULT false,
     muted_role_id bigint,
 
-    CONSTRAINT guild_settings_id_foreign FOREIGN KEY(id) REFERENCES guild(guild_id)
+    CONSTRAINT guild_settings_id_foreign FOREIGN KEY(id) REFERENCES guild(id)
 );
 
 CREATE TABLE "warning"
@@ -34,9 +34,9 @@ CREATE TABLE "warning"
     issued_guild bigint NOT NULL,
     reason varchar(255),
 
-    CONSTRAINT warning_issued_by_foreign FOREIGN KEY(issued_by) REFERENCES "user"(user_id),
-    CONSTRAINT warning_issued_to_foreign FOREIGN KEY(issued_to) REFERENCES "user"(user_id),
-    CONSTRAINT warning_issued_guild_foreign FOREIGN KEY(issued_guild) REFERENCES guild(guild_id)
+    CONSTRAINT warning_issued_by_foreign FOREIGN KEY(issued_by) REFERENCES "user"(id),
+    CONSTRAINT warning_issued_to_foreign FOREIGN KEY(issued_to) REFERENCES "user"(id),
+    CONSTRAINT warning_issued_guild_foreign FOREIGN KEY(issued_guild) REFERENCES guild(id)
 );
 
 CREATE TABLE mute
@@ -49,9 +49,9 @@ CREATE TABLE mute
     expires timestamp without time zone NOT NULL,
     reason varchar(255),
 
-    CONSTRAINT mute_issued_by_foreign FOREIGN KEY(issued_by) REFERENCES "user"(user_id),
-    CONSTRAINT mute_issued_to_foreign FOREIGN KEY(issued_to) REFERENCES "user"(user_id),
-    CONSTRAINT mute_issued_guild_foreign FOREIGN KEY(issued_guild) REFERENCES guild(guild_id)
+    CONSTRAINT mute_issued_by_foreign FOREIGN KEY(issued_by) REFERENCES "user"(id),
+    CONSTRAINT mute_issued_to_foreign FOREIGN KEY(issued_to) REFERENCES "user"(id),
+    CONSTRAINT mute_issued_guild_foreign FOREIGN KEY(issued_guild) REFERENCES guild(id)
 );
 
 CREATE TABLE kick
@@ -63,9 +63,9 @@ CREATE TABLE kick
     issued_guild bigint NOT NULL,
     reason varchar(255),
 
-    CONSTRAINT kick_issued_by_foreign FOREIGN KEY(issued_by) REFERENCES "user"(user_id),
-    CONSTRAINT kick_issued_to_foreign FOREIGN KEY(issued_to) REFERENCES "user"(user_id),
-    CONSTRAINT kick_issued_guild_foreign FOREIGN KEY(issued_guild) REFERENCES guild(guild_id)
+    CONSTRAINT kick_issued_by_foreign FOREIGN KEY(issued_by) REFERENCES "user"(id),
+    CONSTRAINT kick_issued_to_foreign FOREIGN KEY(issued_to) REFERENCES "user"(id),
+    CONSTRAINT kick_issued_guild_foreign FOREIGN KEY(issued_guild) REFERENCES guild(id)
 );
 
 CREATE TABLE ban
@@ -78,7 +78,8 @@ CREATE TABLE ban
     expires timestamp without time zone NOT NULL DEFAULT '2269-01-01 00:00:00'::timestamp without time zone,
     reason varchar(255),
 
-    CONSTRAINT ban_issued_by_foreign FOREIGN KEY(issued_by) REFERENCES "user"(user_id),
-    CONSTRAINT ban_issued_to_foreign FOREIGN KEY(issued_to) REFERENCES "user"(user_id),
-    CONSTRAINT ban_issued_guild_foreign FOREIGN KEY(issued_guild) REFERENCES guild(guild_id)
+    CONSTRAINT ban_issued_by_foreign FOREIGN KEY(issued_by) REFERENCES "user"(id),
+    CONSTRAINT ban_issued_to_foreign FOREIGN KEY(issued_to) REFERENCES "user"(id),
+    CONSTRAINT ban_issued_guild_foreign FOREIGN KEY(issued_guild) REFERENCES guild(id)
+);
 );
